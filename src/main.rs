b599885b76ee1e506f7ca925fa31481b5e14032a
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-06-19 16:25:54 +0900

    ref:メンテナンス性向上のため、ファイルを分割

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 extern crate glium;
 extern crate image;
 
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 #[path="./teapot.rs"]
 mod teapot;
 
@@ -18,39 +22,13 @@ fn main() {
     let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
     let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &teapot::INDICES).unwrap();
 
-    let vertex_shader_src = r#"
-        #version 150
-
-        in vec3 position;
-        in vec3 normal;
-
-        out vec3 v_normal;
-
-        uniform mat4 matrix;
-
-        void main() {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;
-            gl_Position = matrix * vec4(position, 1.0);
-        }
-    "#;
-
-    let fragment_shader_src = r#"
-        #version 140
-
-        in vec3 v_normal;
-        out vec4 color;
-        uniform vec3 u_light;
-
-        void main() {
-            float brightness = dot(normalize(v_normal), normalize(u_light));
-            vec3 dark_color = vec3(0.6, 0.0, 0.0);
-            vec3 regular_color = vec3(1.0, 0.0, 0.0);
-            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
-        }
-    "#;
+    // main.vertを読み込む
+    let vertex_shader_src = include_str!("./main.vert");
+    // main.fragを読み込む
+    let fragment_shader_src = include_str!("./main.frag");
 
     let program =
-        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+        glium::Program::from_source(&display, &vertex_shader_src, fragment_shader_src, None)
             .unwrap();
 
     let mut t: f32 = -0.5;
