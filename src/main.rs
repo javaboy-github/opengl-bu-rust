016119618bd08a93d68677a97fbd71741662390c
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-07-02 19:18:42 +0900

    add:camera

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,46 @@ extern crate image;
 #[path = "./teapot.rs"]
 mod teapot;
 
+fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [
+        up[1] * f[2] - up[2] * f[1],
+        up[2] * f[0] - up[2] * f[2],
+        up[0] * f[1] - up[1] * f[0],
+    ];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [
+        f[1] * s_norm[2] - f[2] * s_norm[1],
+        f[2] * s_norm[0] - f[0] * s_norm[2],
+        f[0] * s_norm[1] - f[1] * s_norm[0],
+    ];
+
+    let p = [
+        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+        -position[0] * f[0] - position[1] * f[1] - position[2] * u[2],
+    ];
+
+    [
+        [s_norm[0], u[0], f[0], 0.0],
+        [s_norm[1], u[1], f[1], 0.0],
+        [s_norm[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
+}
+
 fn main() {
     #[allow(unused_imports)]
     use glium::{glutin, Surface};
@@ -32,7 +72,7 @@ fn main() {
         glium::Program::from_source(&display, &vertex_shader_src, fragment_shader_src, None)
             .unwrap();
 
-    let mut t: f32 = -0.5;
+    let mut view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
     event_loop.run(move |event, _, control_flow| {
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
@@ -57,42 +97,37 @@ fn main() {
             std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
-        // 「t」を更新
-        t += 0.0002;
-        if t > 0.5 {
-            t = -0.5;
-        }
-
         let mut target = display.draw();
         target.clear_color_and_depth((0.0, 0., 1.0, 1.0), 1.0);
 
         let uniforms = uniform! {
-            matrix: [
+            model: [
                 [0.01, 0.0, 0.0, 0.0],
                 [0.0, 0.01, 0.0, 0.0],
                 [0.0, 0.0, 0.01, 0.0],
                 [0.0, 0.0, 2.0, 1.0f32],
             ],
+            view:view,
             u_light: [-1.0, 0.4, 0.9f32],
-                perspective: {
+            perspective: {
 
-                    let (width, height) = target.get_dimensions();
-                    let aspect_ratio = height as f32 / width as f32;
+                let (width, height) = target.get_dimensions();
+                let aspect_ratio = height as f32 / width as f32;
 
 
-                    let fov: f32 = 3.141592 / 3.0;
-                    let zfar = 1024.0;
-                    let znear = 0.1;
+                let fov: f32 = 3.141592 / 3.0;
+                let zfar = 1024.0;
+                let znear = 0.1;
 
-                    let f = 1.0/ (fov/ 2.0).tan();
+                let f = 1.0/ (fov/ 2.0).tan();
 
-                    [
-                        [f * aspect_ratio, 0.0, 0.0, 0.0],
-                        [0.0, f, 0.0, 0.0],
-                        [0.0, 0.0, (zfar+znear)/ (zfar-znear), 1.0],
-                        [0.0, 0.0, -(2.0*zfar*znear)/(zfar-znear), 0.0]
-                    ]
-                }
+                [
+                    [f * aspect_ratio, 0.0, 0.0, 0.0],
+                    [0.0, f, 0.0, 0.0],
+                    [0.0, 0.0, (zfar+znear)/ (zfar-znear), 1.0],
+                    [0.0, 0.0, -(2.0*zfar*znear)/(zfar-znear), 0.0]
+                ]
+            }
         };
         let params = glium::DrawParameters {
             depth: glium::Depth {
