e3fb798101459834f68506a7c44f89d08590e2e9
javaboy-github <75339009+javaboy-github@users.noreply.github.com>
2021-07-17 14:47:30 +0900

    ref: 公式のexampleのCameraを使用
    本当に訳がわからなかったので、公式からとってきた

--- src/main.rs
+++ src/main.rs
@@ -5,61 +5,11 @@ extern crate image;
 #[path = "./teapot.rs"]
 mod teapot;
 
+#[path = "./camera.rs"]
+mod camera;
+
 use cgmath::Vector3;
 
-fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-    use cgmath::ElementWise;
-    /// ベクトル内の要素の合計を返す
-    /// # example
-    /// ```rust
-    /// assert_eq!(sum(Vector3::new(1, 2, 3)), 6);
-    /// ```
-    fn sum(vec: Vector3<f32>) -> f32 {
-        return vec.x + vec.y + vec.z;
-    }
-
-    let position = Vector3::new(position[0], position[1], position[2]);
-    let direction = Vector3::new(direction[0], direction[1], direction[2]);
-    let up = Vector3::new(up[0], up[1], up[2]);
-
-    let f = {
-        let f = direction;
-        let len = sum(f.map(|e| e.powi(2)));
-        let len = len.sqrt();
-        f.map(|e| e / len)
-    };
-
-    let s = Vector3::new(
-        up[1] * f[2] - up[2] * f[1],
-        up[2] * f[0] - up[0] * f[2],
-        up[0] * f[1] - up[1] * f[0],
-    );
-
-    let s_norm = {
-        let len = sum(s.map(|e| e.powi(2)));
-        let len = len.sqrt();
-        s.map(|e| e / len)
-    };
-
-    let u = Vector3::new(
-        f[1] * s_norm[2] - f[2] * s_norm[1],
-        f[2] * s_norm[0] - f[0] * s_norm[2],
-        f[0] * s_norm[1] - f[1] * s_norm[0],
-    );
-
-    let p = Vector3::new(
-        sum(-position.mul_element_wise(s_norm)),
-        sum(-position.mul_element_wise(u)),
-        sum(-position.mul_element_wise(f)),
-    );
-
-    [
-        [s_norm[0], u[0], f[0], 0.0],
-        [s_norm[1], u[1], f[1], 0.0],
-        [s_norm[2], u[2], f[2], 0.0],
-        [p[0], p[1], p[2], 1.0],
-    ]
-}
 
 fn main() {
     #[allow(unused_imports)]
@@ -88,30 +38,20 @@ fn main() {
         glium::Program::from_source(&display, &vertex_shader_src, fragment_shader_src, None)
             .unwrap();
 
-    // カメラの表示 [位置, 方向, 画面上部のシーン座標の方向]
-    // 位置は、ティーポットを中心に置くための値
-    // 方向は、シーン座標をカメラが向いている
-    // 画面上部のシーン座標の方向は、　スクリーンのトップのシーン座標の方向を表す
-    let mut original_view = [[2.0, -1.0, 0.0], [-2.0, 1.0, 1.0], [0.0, 1.0, 0.0]];
-    let mut old_cursor_coords = glium::glutin::dpi::PhysicalPosition { x: 0.0, y: 0.0 };
+    let mut camera = camera::CameraState::new();
+    camera.set_position((2.0, -1.0, 0.0));
+    camera.set_direction((-2.0, 1.0, 1.0));
+
     event_loop.run(move |event, _, control_flow| {
+        camera.update();
         match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
                     return;
                 }
-                glutin::event::WindowEvent::CursorMoved { position, .. } => {
-                    let x = position.x;
-                    let y = position.y;
-                    let dx = old_cursor_coords.x - x; // different x
-                    let dy = old_cursor_coords.y - y; // different x
-                    original_view[1][2] -= dx as f32 / 10f32;
-                    original_view[1][0] -= dy as f32 / 10f32;
-                    old_cursor_coords = position;
-                }
-                glutin::event::WindowEvent::KeyboardInput { input, .. } => {
-                    let motion = 1f32;
+                glutin::event::WindowEvent::KeyboardInput {..} => {
+                    camera.process_input(&event);
                 }
                 _ => return,
             },
@@ -137,26 +77,9 @@ fn main() {
                 [0.0, 0.0, 0.01, 0.0],
                 [0.0, 0.0, 2.0, 1.0f32],
             ],
-            view: view_matrix(&original_view[0], &original_view[1], &original_view[2]),
+            view: camera.get_view(),
             u_light: [-1.0, 0.4, 0.9f32],
-            perspective: {
-                let (width, height) = target.get_dimensions();
-                let aspect_ratio = height as f32 / width as f32;
-
-
-                let fov: f32 = 3.141592 / 3.0;
-                let zfar = 1024.0;
-                let znear = 0.1;
-
-                let f = 1.0/ (fov/ 2.0).tan();
-
-                [
-                    [f * aspect_ratio, 0.0, 0.0, 0.0],
-                    [0.0, f, 0.0, 0.0],
-                    [0.0, 0.0, (zfar+znear)/ (zfar-znear), 1.0],
-                    [0.0, 0.0, -(2.0*zfar*znear)/(zfar-znear), 0.0]
-                ]
-            }
+            perspective: camera.get_perspective(),
         };
         let params = glium::DrawParameters {
             depth: glium::Depth {
@@ -179,21 +102,4 @@ fn main() {
             .unwrap();
         target.finish().unwrap();
     });
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    /// `view_matrix`がしっかり動くかのテスト
-    fn does_view_matrix_method_work() {
-        assert_eq!(
-            view_matrix(&[2.0, -1.0, 0.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]),
-            [[0.4472136, 0.36514837, -0.81649655, 0.0],
-            [0.0, 0.9128709, 0.40824828, 0.0],
-            [0.8944272, -0.18257418, 0.40824828, 0.0],
-            [-0.8944272, 0.18257415, 2.0412414, 1.0]],
-        );
-    }
 }
